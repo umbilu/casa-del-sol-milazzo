1ebe27d2208ec82fae0ffb65191c104500309109
umbilu <36154636+umbilu@users.noreply.github.com>
2026-02-04 15:45:27 +0100

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
       {/* Background Image */}
       <div className="absolute inset-0 z-0">
         <img
-          src="{require('./src/images/milazzo.jpeg')}"
+          src="{'/src/images/milazzo.jpeg'}"
           alt="Sicilia Landscape"
           className="w-full h-full object-cover brightness-[0.55]"
         />
